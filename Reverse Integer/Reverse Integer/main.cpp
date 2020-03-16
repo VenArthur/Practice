b60776424825c009f525e4b60ed4090347134b08
VenArthur <60660305+VenArthur@users.noreply.github.com>
2020-03-16 16:50:33 -0400

    Leetcode Reverse Integer Solution in C++

---
+++ Reverse Integer/Reverse Integer/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+//Given a 32-bit signed integer, reverse digits of an integer.
+int reverse(int x);
+
+int main()
+{
+	//-2^31 = -2147483648, 2^31 = 2147483648
+
+	std::cout << reverse(-147483648);
+
+	system("PAUSE");
+	return 0;
+}
+
+int reverse(int x)
+{
+	std::string number;
+	std::string reversedNumber;
+	bool isNegative = false;
+
+	//Will need for after type conversion
+	if (x < 0)
+	{
+		isNegative = true;
+	}
+	else
+	{
+		isNegative = false;
+	}
+
+	//Convert the number from int to string 
+	number = std::to_string(x);
+
+	//Reverse the number
+	for (int i = number.length() - 1; i >= 0; i--)
+	{
+		reversedNumber.push_back(number[i]);
+	}
+
+	//Convert the number from string back to int
+	int answer;
+	std::istringstream(reversedNumber) >> answer;
+
+	//(INT_MIN)-2^31 = -2147483648, (INT_MAX)2^31 = 2147483648 - if answer is out of range return 0
+	if (answer == INT_MAX || answer == INT_MIN)
+	{
+		return 0;
+	}
+
+	//If the original number input was a negative, the answer must be negative
+	if (isNegative)
+	{
+		answer *= -1;
+	}
+
+
+	return answer;
+}
